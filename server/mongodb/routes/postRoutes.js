86c8db92bf9307feae5d796044dbef6c34855e34
RonElmalech <ronelmalechh@cs.colman.ac.il>
2024-12-01 21:52:11 +0200

    Refactor post creation to use secure URLs from Cloudinary for image uploads

--- server/mongodb/routes/postRoutes.js
+++ server/mongodb/routes/postRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
-
 import Post from '../models/post.js';
 
 dotenv.config();
@@ -33,20 +32,22 @@ router.post('/', async (req, res) => {
         // Check if the photo is base64 or a URL
         if (photo) {
             let photoUrl;
-
             if (photo.startsWith('data:image')) {
                 // Upload base64 image
                 photoUrl = await cloudinary.uploader.upload(photo, { resource_type: 'auto' });
             } else if (photo.startsWith('http')) {
-                // Upload image from URL (this will also return an HTTPS URL)
+                // Upload image from URL
                 photoUrl = await cloudinary.uploader.upload(photo);
             }
 
-            // Create the post with the secure image URL (Cloudinary should return HTTPS by default)
+            // Use the secure URL directly from Cloudinary
+            const securePhotoUrl = photoUrl.secure_url;
+
+            // Create the post with the secure image URL
             const newPost = await Post.create({
                 name,
                 prompt,
-                photo: photoUrl.url, // Use the returned Cloudinary URL
+                photo: securePhotoUrl, // Use the secure URL
             });
             res.status(201).json({ success: true, data: newPost });
         }
