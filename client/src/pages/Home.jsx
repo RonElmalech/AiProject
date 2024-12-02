6ae315520ce0399959c5d165e3c5195d7242a7bf
RonElmalech <ronelmalechh@cs.colman.ac.il>
2024-12-02 04:00:48 +0200

    Update grid layout in Home component for improved responsiveness

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -88,7 +88,7 @@ const Home = () => {
                 Showing results for <span className='text-[#222328]'>{searchText}</span>
               </h2>
             )}
-            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
               {searchText ? (
                 <RenderCards data={searchedResults} title='No search results found' />
               ) : (
